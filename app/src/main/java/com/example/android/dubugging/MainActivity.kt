cfe922d5d5eb4e5ffea386bad9cd5bebdb72d392
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-21 00:06:23 +0900

    add NullPointerException

--- app/src/main/java/com/example/android/dubugging/MainActivity.kt
+++ app/src/main/java/com/example/android/dubugging/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.android.dubugging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val helloText: TextView = findViewById(R.id.hello_world)
+        helloText.text = "Hello debugging"
         setContentView(R.layout.activity_main)
         logging()
         division()
@@ -22,8 +25,9 @@ fun logging() {
 fun division() {
     val numerator = 60
     var denominator = 4
-    repeat(5) {
+    repeat(4) {
         Log.v(TAG, "${numerator/denominator}")
+        Log.d(TAG, "$denominator")
         denominator--
     }
 }
