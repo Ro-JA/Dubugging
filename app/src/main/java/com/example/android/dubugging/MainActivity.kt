2cc3f23e79f9bd88c2dd8413d892b5cdba68f2cc
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-21 00:55:48 +0900

    add screenshot and record

--- app/src/main/java/com/example/android/dubugging/MainActivity.kt
+++ app/src/main/java/com/example/android/dubugging/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import kotlin.concurrent.thread
 
 private const val TAG = "MainActivity"
 
@@ -11,25 +12,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "this is where the app crashed before")
-        val helloText: TextView = findViewById(R.id.hello_world)
-        Log.d(TAG, "this should be logged if the bug is fixed ")
-        helloText.text = "Hello debugging"
-        logging()
         division()
     }
-}
 
-fun logging() {
-    Log.v(TAG, "Hello World!")
-}
 
-fun division() {
-    val numerator = 60
-    var denominator = 4
-    repeat(4) {
-        Log.v(TAG, "${numerator/denominator}")
-        Log.d(TAG, "$denominator")
-        denominator--
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        thread(start = true) {
+            repeat(4) {
+                Thread.sleep(3000)
+                runOnUiThread {
+                    findViewById<TextView>(R.id.division_text_view).setText("${numerator / denominator}")
+                    denominator--
+                }
+            }
+        }
     }
 }
+
